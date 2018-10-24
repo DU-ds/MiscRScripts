869f0d440a982af76bfc96c5faac86c8955c0265
DU-ds <kjcolins1759@gmail.com>
2018-10-24 15:30:19 -0400

    "updated file to include other matrix, also fixed off by one error" M  matrixMult100.r

--- matrixMult100.r
+++ matrixMult100.r
@@ -1,6 +1,6 @@
 A <- matrix(c(3,1,4,1),ncol = 2)
 mat <- A 
-for(i in 1:100){ 
+for(i in 1:99){ 
 	mat <- (mat %*% A)
 }
 print(mat)
@@ -9,3 +9,14 @@ print(mat)
 # inspired by:
 # https://www.youtube.com/watch?v=PFDu9oVAE-g&list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab&index=13
 # 14 min, 30 seconds
+
+
+B <- matrix(c(3,0,0,2), ncol = 2)
+mat2 <- B
+for(i in 1:99){
+	mat2 <- (mat2 %*% B)
+}
+print(mat2)
+
+mat2[1,1] == 3^100 #true
+mat2[2,2] == 2^100 #true
