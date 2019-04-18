1b83c640d72208f62bfbccee5e16436dd8b73811
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-04-18 11:14:53 -0400

    "cleaned up code"

--- HackerRankBinomialDistribution1.R
+++ HackerRankBinomialDistribution1.R
@@ -1,21 +1,14 @@
 # https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem
 input <- c(1.09, 1)
-boy <- input[1]
-girl <- input[2]
+one <- input[1]
+two <- input[2]
 numChildren <- 6
 numTrials <- 100000
-# boy/girl
-prboy <- boy/(girl+boy) 
-prgirl <- girl/(girl+boy)
-# ntrialsboy <- rbinom(numTrials, numChildren, prboy)
-# threeormoreboys <- ifelse(ntrialsboy <= 3, 1, 0)
-# ans <- sum(threeormoreboys) / numTrials
-# http://127.0.0.1:27454/library/stats/html/Binomial.html
-# cat(sprintf("%.3f", ans))
+pr <- two/(two+one)
 
-
-ntrialsgirls <- rbinom(numTrials, numChildren, prgirl)
-threeormoregirls <- ifelse(ntrialsgirls <= 3, 1, 0)
-ans <- sum(threeormoregirls) / numTrials
+trials <- rbinom(numTrials, numChildren, pr)
+threeormore <- ifelse(trials <= 3, 1, 0)
+ans <- sum(threeormore) / numTrials
 cat(sprintf("%.3f", ans))
+
 
