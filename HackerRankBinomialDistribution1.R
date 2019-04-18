72398b615019d8ce26157ab6327d48c0b1d900ad
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-04-18 12:06:43 -0400

    "more code for better understanding"

--- HackerRankBinomialDistribution1.R
+++ HackerRankBinomialDistribution1.R
@@ -3,7 +3,7 @@ input <- c(1.09, 1)
 one <- input[1]
 two <- input[2]
 numChildren <- 6
-numTrials <- 100000
+numTrials <- 1000000
 pr <- two/(two+one)
 
 trials <- rbinom(numTrials, numChildren, pr)
@@ -12,3 +12,16 @@ ans <- sum(threeormore) / numTrials
 cat(sprintf("%.3f", ans))
 
 
+
+pr1 <- one/(two + one)
+trials1 <-  rbinom(numTrials, numChildren, pr1)
+threeormore <- ifelse(trials1 <= 3, 1, 0)
+ans1 <- sum(threeormore) / numTrials
+cat(sprintf("%.3f", ans))
+
+threeormore <- ifelse(trials1 == 3, 1, 0)
+ans3 <- sum(threeormore) / numTrials
+cat(sprintf("%.3f", ans))
+
+cat(sprintf("%.3f", (ans + ans1 - ans3))) 
+#1.000
