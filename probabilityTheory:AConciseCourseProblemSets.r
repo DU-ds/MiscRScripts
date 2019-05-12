f4862b117cdea6fe8b02c7412d9bd2707b6a9a43
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-05-12 17:42:08 -0400

    "Chapter 2"

--- probabilityTheory:AConciseCourseProblemSets.r
+++ probabilityTheory:AConciseCourseProblemSets.r
@@ -55,3 +55,63 @@ n <- 2^50
 N <- chooseWIthStirling(100,50)
 n/N
 
+#Chapter 2
+#1 
+# a) AB = A
+# Since AB = A intersection B, AB = A --> A = B
+
+# b) ABC = A --> A = B = C
+
+# c) A union B union C = A --> B is a subset of A and C is a subset of A
+
+# 2
+# a) A union B = not A
+# B = 
+
+# by the identity if A = A1 union A2, then not A = not A1 union not A2
+# not not A = not A union not B
+# A = not A union not B
+# Since for any set L, L intersection not L = empty set
+# and L union not L = Omega  
+# Let the sample space be Omega. then, not A = Omega - A
+# A union B = not A --> A union B = Omega - A
+# if A = empty set, then Omega = B
+# if A =/= empty set, then A = not A intersection not B
+# it seems that the only way this is true is if
+# A is the empty set and B is Omega
+
+# b) AB = not A
+
+# 3
+
+# 5
+# A union B = Omega
+# AB = empty set
+
+
+# 15
+# Test the approximation (2.14) for n = 3,4,5,6
+
+two.14 <- function(n){
+	sum <- 0
+	for(i in 1:n){
+		sum <- sum - (((-1)^i)/factorial(i))
+	}
+    return(sum)
+}
+
+approxtwo.14 <- 1- exp(-1)
+
+(n3 <- two.14(3))
+(n4 <- two.14(4))
+(n5 <- two.14(5))
+(n6 <- two.14(6))
+
+
+(n3 - approxtwo.14)
+(n4 - approxtwo.14)
+(n5 - approxtwo.14)
+(n6 - approxtwo.14)
+
+cat(n3 - approxtwo.14, "\n", n4 - approxtwo.14, "\n", n5 - approxtwo.14, "\n", n6 - approxtwo.14, "\n")
+
